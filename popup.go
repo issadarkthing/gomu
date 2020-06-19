05b4f823752455045ce3996142cbc5a9a45126ff
raziman <raziman.terra@gmail.com>
2020-06-19 16:22:20 +0800

    structure project

---
+++ popup.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
+)
+
+func confirmationPopup(
+	app *tview.Application,
+	pages *tview.Pages,
+	text string,
+	handler func (buttonIndex int, buttonLabel string),
+) {
+
+
+	modal := tview.NewModal().
+				SetText(text).
+				SetBackgroundColor(tcell.ColorDefault).
+				AddButtons([]string{"yes", "no"}).
+				SetButtonBackgroundColor(tcell.ColorBlack).
+				SetDoneFunc(handler);
+
+
+	pages.AddPage("confirmation-popup", center(modal, 40, 10), true, true)
+	app.SetFocus(modal)
+
+}
+
+func center(p tview.Primitive, width, height int) tview.Primitive {
+	return tview.NewFlex().
+			AddItem(nil, 0, 1, false).
+			AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+				AddItem(nil, 0, 1, false).
+				AddItem(p, height, 1, false).
+				AddItem(nil, 0, 1, false), width, 1, false).
+			AddItem(nil, 0, 1, false)
+}
---
+++ utils.go
@@ -0,0 +1,21 @@
+package main
+
+import "os"
+
+func log(text string) {
+
+	f, err := os.OpenFile("message.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := f.Write([]byte(text)); err != nil {
+		panic(err)
+	}
+
+	if err := f.Close(); err != nil {
+		panic(err)
+	}
+
+}
