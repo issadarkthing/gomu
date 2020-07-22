9e2e9980187ef201d387ab176b21d55cfb24830c
raziman <raziman.terra@gmail.com>
2020-07-22 21:00:44 +0800

    remove init function

--- gomu.go
+++ gomu.go
@@ -3,9 +3,7 @@
 package main
 
 import (
-	"log"
 	"os"
-	"path"
 
 	"github.com/rivo/tview"
 )
@@ -22,22 +20,4 @@ func main() {
 
 	start(app, args)
 
-}
-
-func init() {
-
-	tmpDir := os.TempDir()
-
-	logFile := path.Join(tmpDir, "gomu.log")
-
-	file, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-
-	if err != nil {
-		log.Fatalf("Error opening file %s", logFile)
-	}
-
-	defer file.Close()
-
-	log.SetOutput(file)
-	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
 }
