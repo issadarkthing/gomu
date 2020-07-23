da80ea71280755ad713dd3294fb7a07c0272cbb5
raziman <raziman.terra@gmail.com>
2020-07-23 15:15:54 +0800

    new entry point

---
+++ main.go
@@ -0,0 +1,17 @@
+// Copyright (C) 2020  Raziman
+
+package main
+
+import (
+	"os"
+
+	"github.com/rivo/tview"
+)
+
+func main() {
+	os.Setenv("TEST", "false")
+	args := getArgs()
+	readConfig(args)
+	app := tview.NewApplication()
+	start(app, args)
+}
