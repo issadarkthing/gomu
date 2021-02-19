4fa99d10438d95db96025efc54cab3778f376708
raziman <raziman.terra@gmail.com>
2021-02-19 12:08:08 +0800

    add more tests

--- hook/hook_test.go
+++ hook/hook_test.go
@@ -6,7 +6,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRunHook(t *testing.T) {
+func TestAddHook(t *testing.T) {
+	
+	h := NewEventHook()
+
+	h.AddHook("a", nil)
+	h.AddHook("a", nil)
+	h.AddHook("a", nil)
+	h.AddHook("a", nil)
+
+	assert.Equal(t, 1, len(h.events), "should only contain 1 event")
+
+	hooks := h.events["a"]
+	assert.Equal(t, 4, len(hooks), "should contain 4 hooks")
+
+	h.AddHook("b", nil)
+	h.AddHook("c", nil)
+
+	assert.Equal(t, 3, len(h.events), "should contain 3 events")
+}
+
+func TestRunHooks(t *testing.T) {
 
 	h := NewEventHook()
 	i := 0
@@ -19,13 +39,17 @@ func TestRunHook(t *testing.T) {
 		i++
 	})
 
-	assert.Equal(t, i, 0)
+	h.AddHook("noop", func() {
+		i++
+	})
+
+	assert.Equal(t, i, 0, "should not execute any hook")
 
 	h.RunHooks("x")
 
-	assert.Equal(t, i, 0)
+	assert.Equal(t, i, 0, "should not execute any hook")
 
 	h.RunHooks("sample")
 
-	assert.Equal(t, i, 2)
+	assert.Equal(t, i, 2, "should only execute event 'sample'")
 }
