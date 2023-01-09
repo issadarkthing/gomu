6e7167120aebc4380f990e9017b0fd83b7d5a7e4
154pinkchairs <mholub@tutanota.com>
2023-01-09 03:47:08 +0100

    test(start): add unit test for getArgs

--- start_test.go
+++ start_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"testing"
 
 	"github.com/issadarkthing/gomu/anko"
@@ -10,6 +13,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Test default case
+func TestGetArgsDefaults(t *testing.T) {
+	args := getArgs()
+	assert.Equal(t, *args.config, "~/.config/gomu/config")
+	assert.Equal(t, *args.empty, false)
+	assert.Equal(t, *args.music, "~/music")
+	assert.Equal(t, *args.version, false)
+}
+
+// Test non-standard flags/the empty/version flags
 func TestGetArgs(t *testing.T) {
 	home, err := os.UserHomeDir()
 	if err != nil {
@@ -21,12 +34,6 @@ func TestGetArgs(t *testing.T) {
 		t.Error(err)
 		t.FailNow()
 	}
-	// Test default case
-	ar := getArgs()
-	assert.Equal(t, *ar.config, "~/.config/gomu/config")
-	assert.Equal(t, *ar.empty, false)
-	assert.Equal(t, *ar.music, "~/music")
-	assert.Equal(t, *ar.version, false)
 
 	// Test setting config flag
 	testConfig := filepath.Join(cfgDir, ".tmp", "gomu")
@@ -41,31 +48,45 @@ func TestGetArgs(t *testing.T) {
 		t.Error(err)
 		t.FailNow()
 	}
-	os.Args = []string{"cmd", "-config", tmpCfgf.Name()}
-	ar = getArgs()
-	assert.Equal(t, *ar.config, tmpCfgf.Name())
 
-	// Test -empty flag
-	os.Args = []string{"cmd", "-empty"}
-	ar = getArgs()
-	assert.Equal(t, *ar.empty, true)
-	assert.Zero(t, gomu.queue) // not sure if that's correct
-
-	// Test setting music flag
 	testMusic := filepath.Join(home, ".tmp", "gomu")
 	_, err = os.Stat(testMusic)
 	if os.IsNotExist(err) {
 		os.MkdirAll(testMusic, 0755)
 	}
 	defer os.RemoveAll(testMusic)
-	os.Args = []string{"cmd", "-music", testMusic}
-	ar = getArgs()
-	assert.Equal(t, *ar.music, testMusic)
 
-	// Test the usage of version flag
-	os.Args = []string{"cmd", "-version"}
-	ar = getArgs()
-	assert.Equal(t, *ar.version, true)
+	boolChecks := []struct {
+		name string
+		arg  bool
+		want bool
+	}{
+		{"empty", true, true},
+		{"version", true, true},
+	}
+	for _, check := range boolChecks {
+		t.Run("testing bool flag "+check.name, func(t *testing.T) {
+			flag.CommandLine.Set(check.name, strconv.FormatBool(check.arg))
+			flag.CommandLine.Parse(os.Args[1:])
+			assert.Equal(t, check.arg, check.want)
+		})
+	}
+	strChecks := []struct {
+		name string
+		arg  string
+		want string
+	}{
+		{"config", tmpCfgf.Name(), tmpCfgf.Name()},
+		{"music", testMusic, testMusic},
+	}
+	for _, check := range strChecks {
+		t.Run("testing string flag "+check.name, func(t *testing.T) {
+			flag.CommandLine.Set(check.name, check.arg)
+			flag.CommandLine.Parse(os.Args[1:])
+			fmt.Println("flag value: ", check.arg)
+			assert.Equal(t, check.arg, check.want)
+		})
+	}
 }
 
 func TestSetupHooks(t *testing.T) {
